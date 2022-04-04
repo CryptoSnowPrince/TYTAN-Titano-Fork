3dafc0a84dfb4627372213bbdcdf31eaa6b26d52
CryptoSnowMickey <topdirector2017@gmail.com>
2022-04-04 22:36:51 +0900

    update

---
+++ Frontend/src/contract/private.example.js
@@ -0,0 +1,7 @@
+const privateInfo = {
+    REACT_APP_INFURA_ID: "Your Infura ID",
+    REACT_APP_PRIVATE_KEY: "Your Private Key",
+    REACT_APP_API_KEY: "Your API Key"
+}
+
+export default privateInfo;
